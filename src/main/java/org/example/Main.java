73d02ee7e3d20f7e73d80717586765c617ee105d
User <danilgaybeshev@mail.ru>
2025-02-24 20:27:21 +0500

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author stephenKing = new Author("Стивен", "Кинг");
+        Book it = new Book("Оно", stephenKing, 1986);
+        System.out.println("it.title = " + it.getTitle());
+        System.out.println("it.author = " + it.getAuthor());
+        System.out.println("it.yearOfPublication = " + it.getYearOfPublication());
+        it.setYearOfPublication(1990);
+        System.out.println("it.getYearOfPublication = " + it.getYearOfPublication());
     }
 }
